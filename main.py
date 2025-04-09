9593021dd63fa826f7109c64ee2574ec4c102d72
Izzy Clarke <iclarke2@students.kennesaw.edu>
2025-04-09 14:49:40 -0400

    Update main.py
    added mac vs windows hosts paths and the function to add selected websites to be blocked

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ import sys
 import pywinstyles
 import darkdetect
 
+"""
+Hosts Paths On Windows, Mac & Linux:
+
+Windows: C:\Windows\System32\drivers\etc\hosts
+
+Mac & Linux: /etc/hosts
+"""
+
+h_path = "ENTER HOSTS FILE PATH HERE"
+
+redirect = "127.0.0.1"
+
+website_arr = []
+
+def web_add():
+
+    website_arr.append(website_entry.get())
+
+    print(website_arr)
