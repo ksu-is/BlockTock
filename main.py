5ee8591a6d3b63b315eaac6a3ab531028bcdfb03
Izzy Clarke <iclarke2@students.kennesaw.edu>
2025-04-14 11:59:27 -0400

    Update main.py
    added comments explaining blocks of code for ease of use

--- main.py
+++ main.py
@@ -13,28 +13,32 @@ Windows: C:\Windows\System32\drivers\etc\hosts
 
 Mac & Linux: /etc/hosts
 """
-
+# Path to the hosts file on operating system (change accordingly for Windows/Linux/Mac)
 h_path = "ENTER HOSTS FILE PATH HERE"
 
+# Redirect IP to block websites
 redirect = "127.0.0.1"
 
+# Array to store websites to block
 website_arr = []
 
+# Function to add entered website to the list
 def web_add():
 
-    website_arr.append(website_entry.get())
+    website_arr.append(website_entry.get()) # Get text from entry box and add to array
 
-    print(website_arr)
+    print(website_arr) # Print the list for debugging
 
+# Function to block websites by adding entries to the hosts file
 def block():
 
-    path = open(h_path, "r+")
+    path = open(h_path, "r+") # Open the hosts file in read/write mode
 
-    path_r = path.read()
+    path_r = path.read() # Read its contents (not strictly needed here)
 
     for website in website_arr:
 
-        path.write(f"{redirect} {website}\n")
+        path.write(f"{redirect} {website}\n") # Add redirect entry for each website
     
 def unblock():
     
